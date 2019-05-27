f4cf0ad3456efb3aa364be2cc9ac982752eac455
Portia <plburton@gmail.com>
2019-05-27 22:07:44 +0200

    Fixed link in pic

--- src/components/Sidebar/Author/Author.js
+++ src/components/Sidebar/Author/Author.js
@@ -4,7 +4,7 @@ import styles from './Author.module.scss';
 
 const Author = ({ author, isIndex }) => (
   <div className={styles['author']}>
-    <Link to="/services">
+    <Link to="/">
       <img
         src={withPrefix(author.photo)}
         className={styles['author__photo']}
